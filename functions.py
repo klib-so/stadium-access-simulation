152f8f146f2428c8a7ac4c634f7b737fbe896d85
klib <calebgilmartin@gmail.com>
2026-03-25 11:49:37 +0000

    Fixed up distribution functions

--- functions.py
+++ functions.py
@@ -10,40 +10,21 @@ import stadium_entities
 def format_time(timestamp):
     timestamp = math.floor(timestamp)
     return time.strftime("%H:%M:%S", time.gmtime(timestamp))
-    # hour = (time - (time % 10000))//10000
-    # minute = ((time % 10000) - (time % 100))//100
-    # second = time % 100
-    # return f"{hour}:{minute}:{second}"
 
 
-def format_percent(num):
-    adj = '%s' % float('%.2g' % (100 * num/cfg.TICKETS_SOLD))
+def format_percent(numerator, denominator):
+    adj = '%s' % float('%.2g' % (100 * numerator / denominator))
     return f"{adj}%"
 
 
 # Distributions
 def get_service_time(min_wait):
-    return np.random.exponential(scale=5.0, size=None) + min_wait
+    return np.random.exponential(scale=cfg.service_scale, size=None) + min_wait
     # return 5
 
 
-def get_arrival_interval(current_time):
+def get_arrival_interval(population, env_time):
     centre_point = cfg.mean_absolute_arrival_time
-    sd = (cfg.end_time - cfg.start_time) / 6
-    return float(len(cfg.PLAZAS) / (cfg.TICKETS_SOLD * norm.pdf(current_time, loc=centre_point, scale=sd)))
+    sd = cfg.arrival_standard_deviation
+    return norm.ppf((population + 1) / cfg.TICKETS_SOLD, loc=centre_point, scale=sd) - env_time
 
-
-def process_spectator(plaza, spec_id):
-    if plaza.population > 0:
-        stadium_entities.logger.debug(
-            f"{plaza.name} turnstile began processing spectator {spec_id} at {format_time(plaza.env.now)}")
-        yield plaza.env.timeout(get_service_time(cfg.min_service_time))  # Yield to simulate service time
-        admit_spectator(plaza, spec_id)  # Admit the spectator after processing
-
-
-def admit_spectator(plaza, spec_id):
-    stadium_entities.logger.debug(f"Spectator {spec_id} admitted to {plaza.name} at {format_time(plaza.env.now)}")
-    plaza.population -= 1  # Update population count
-    plaza.stadium.population += 1  # Update stadium population if necessary
-    stadium_entities.logger.debug(
-        f"Current stadium population: {format_percent(plaza.stadium.population / cfg.TICKETS_SOLD)}")
